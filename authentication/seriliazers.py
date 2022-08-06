ae61c620f0839493efe7f81e942cf2bf3654764b
dapoadedire <adedireadedapo19@gmail.com>
2022-08-06 14:18:47 +0100

    fix: add email and username field

--- authentication/seriliazers.py
+++ authentication/seriliazers.py
@@ -3,13 +3,17 @@ from authentication.models import User
 
 
 class RegisterSerializer(serializers.Serializer):
-    password = serializers.CharField(max_length=100, min_length=8, write_only=True)
+     username = serializers.CharField(max_length=100, min_length=6)
+     email = serializers.EmailField(max_length=120)
+     password = serializers.CharField(max_length=100, min_length=8, write_only=True)
+  
 
-    class Meta:
-        model = User
-        fields = ["username", "email", "password"]
-    def create(self, validated_data):
-        return User.objects.create_user(**validated_data)
+     class Meta:
+         model = User
+         fields = ["username", "email", "password"]
+     def create(self, validated_data):
+         return User.objects.create_user(**validated_data)
+
 
 
 
