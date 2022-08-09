28ee1a1142fec89a8019dc9ed7989e58ee82159d
dapoadedire <adedireadedapo19@gmail.com>
2022-08-09 18:20:09 +0100

    feat: add whitenoise

--- todolistapi/settings.py
+++ todolistapi/settings.py
@@ -62,12 +62,14 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    
 ]
 
 ROOT_URLCONF = "todolistapi.urls"
